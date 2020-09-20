1b34e8c5ba344b4101a523923685bee29c0c73bb
akasha81 <annapolivana@gmail.com>
2020-09-20 14:55:07 +0100

    add index.blade

---
+++ config/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Youtube</title>
+</head>
+<body>
+    
+<h1>Test Youtube module:</h1>
+
+    {{ trans('youtube::youtube.this_module')}}
+</body>
+</html>
---
+++ config/youtueController.php
@@ -0,0 +1,21 @@
+<?php
+
+&nbsp;
+
+namespace App\Modules\Youtube\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class YoutubeController extends Controller
+
+{
+
+public function index()
+
+{
+
+return view('Youtube::index');
+
+}
+
+}
